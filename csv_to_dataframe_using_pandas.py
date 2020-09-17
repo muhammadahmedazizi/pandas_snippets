439df42353f40d06b71eae7d6845f9191d1f7a62
Muhammad Ahmed <muhammad.ahmed.azizi@gmail.com>
2020-09-17 18:22:17 +0500

    included dictt-to-dataframe and csv-to-dataframe

---
+++ csv_to_dataframe_using_pandas.py
@@ -0,0 +1,8 @@
+# Import pandas as pd
+import pandas as pd
+
+# Import the scrapped_data.csv data: coreyms website data
+df = pd.read_csv('scrapped_data.csv', encoding = "ISO-8859-1") # used this encoding - ISO-8859-1 for utf-8
+
+# Print out dataframe
+print(df)
